ef6012e9960599119871dde0a14359b5583352f8
Rajesh Mule <rajeshsmule@gmail.com>
2020-08-04 17:53:04 +0530

    make a route fetch all topic and topic by id

--- routes/v1/topics.js
+++ routes/v1/topics.js
@@ -10,7 +10,7 @@ const upload = require("../../utils/upload");
 const router = express.Router();
 
 router.post("/", auth.validateJwt, upload.single("image"), createTopic);
-router.get("/", showTopics);
-router.get("/:id", showTopic);
+router.get("/", auth.validateJwt, showTopics);
+router.get("/:id", auth.validateJwt, showTopic);
 
 module.exports = router;
